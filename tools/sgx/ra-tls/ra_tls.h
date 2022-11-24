gramineproject/gramine

[tools/sgx] Cosmetic refactoring of RA-TLS
No functional changes. This is in preparation for "interoperable RA-TLS"
modification of RA-TLS (with IANA-registered OIDs and CBOR tags).

Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

tools/sgx/ra-tls/ra_tls.h
@@ -32,8 +32,8 @@
 
 #define OID(N) \
     { 0x06, 0x09, 0x2A, 0x86, 0x48, 0x86, 0xF8, 0x4D, 0x8A, 0x39, (N) }
-static const uint8_t quote_oid[] = OID(0x06);
-static const size_t quote_oid_len = sizeof(quote_oid);
+static const uint8_t g_quote_oid[] = OID(0x06);
+static const size_t g_quote_oid_size = sizeof(g_quote_oid);
 
 typedef int (*verify_measurements_cb_t)(const char* mrenclave, const char* mrsigner,
                                         const char* isv_prod_id, const char* isv_svn);
@@ -46,11 +46,11 @@ __attribute__ ((visibility("hidden")))
 bool getenv_allow_debug_enclave(void);
 
 __attribute__ ((visibility("hidden")))
-int find_oid(const uint8_t* exts, size_t exts_len, const uint8_t* oid, size_t oid_len,
-             uint8_t** val, size_t* len);
+int cmp_crt_pk_against_quote_report_data(mbedtls_x509_crt* crt, sgx_quote_t* quote);
 
 __attribute__ ((visibility("hidden")))
-int cmp_crt_pk_against_quote_report_data(mbedtls_x509_crt* crt, sgx_quote_t* quote);
+int extract_quote_and_verify_claims(mbedtls_x509_crt* crt, sgx_quote_t** out_quote,
+                                    size_t* out_quote_size);
 
 __attribute__ ((visibility("hidden")))
 int verify_quote_body_against_envvar_measurements(const sgx_quote_body_t* quote_body);

tools/sgx/ra-tls/ra_tls_verify_dcap.c
@@ -118,18 +118,7 @@ int ra_tls_verify_callback(void* data, mbedtls_x509_crt* crt, int depth, uint32_
     /* extract SGX quote from "quote" OID extension from crt */
     sgx_quote_t* quote;
     size_t quote_size;
-    ret = find_oid(crt->v3_ext.p, crt->v3_ext.len, quote_oid, quote_oid_len, (uint8_t**)&quote,
-                   &quote_size);
-    if (ret < 0)
-        goto out;
-
-    if (quote_size < sizeof(*quote)) {
-        ret = MBEDTLS_ERR_X509_INVALID_EXTENSIONS;
-        goto out;
-    }
-
-    /* compare public key's hash from cert against quote's report_data */
-    ret = cmp_crt_pk_against_quote_report_data(crt, quote);
+    ret = extract_quote_and_verify_claims(crt, &quote, &quote_size);
     if (ret < 0)
         goto out;
 

tools/sgx/ra-tls/ra_tls_verify_epid.c
@@ -162,18 +162,7 @@ int ra_tls_verify_callback(void* data, mbedtls_x509_crt* crt, int depth, uint32_
     /* extract SGX quote from "quote" OID extension from crt */
     sgx_quote_t* quote;
     size_t quote_size;
-    ret = find_oid(crt->v3_ext.p, crt->v3_ext.len, quote_oid, quote_oid_len, (uint8_t**)&quote,
-                   &quote_size);
-    if (ret < 0)
-        goto out;
-
-    if (quote_size < sizeof(*quote)) {
-        ret = MBEDTLS_ERR_X509_INVALID_EXTENSIONS;
-        goto out;
-    }
-
-    /* compare public key's hash from cert against quote's report_data */
-    ret = cmp_crt_pk_against_quote_report_data(crt, quote);
+    ret = extract_quote_and_verify_claims(crt, &quote, &quote_size);
     if (ret < 0)
         goto out;
 

forkthus/gramine-tdx

tools/sgx/ra-tls/ra_tls.h
@@ -32,8 +32,8 @@
 
 #define OID(N) \
     { 0x06, 0x09, 0x2A, 0x86, 0x48, 0x86, 0xF8, 0x4D, 0x8A, 0x39, (N) }
-static const uint8_t quote_oid[] = OID(0x06);
-static const size_t quote_oid_len = sizeof(quote_oid);
+static const uint8_t g_quote_oid[] = OID(0x06);
+static const size_t g_quote_oid_size = sizeof(g_quote_oid);
 
 typedef int (*verify_measurements_cb_t)(const char* mrenclave, const char* mrsigner,
                                         const char* isv_prod_id, const char* isv_svn);
@@ -46,11 +46,11 @@ __attribute__ ((visibility("hidden")))
 bool getenv_allow_debug_enclave(void);
 
 __attribute__ ((visibility("hidden")))
-int find_oid(const uint8_t* exts, size_t exts_len, const uint8_t* oid, size_t oid_len,
-             uint8_t** val, size_t* len);
+int cmp_crt_pk_against_quote_report_data(mbedtls_x509_crt* crt, sgx_quote_t* quote);
 
 __attribute__ ((visibility("hidden")))
-int cmp_crt_pk_against_quote_report_data(mbedtls_x509_crt* crt, sgx_quote_t* quote);
+int extract_quote_and_verify_pubkey(mbedtls_x509_crt* crt, sgx_quote_t** out_quote,
+                                    size_t* out_quote_size);
 
 __attribute__ ((visibility("hidden")))
 int verify_quote_body_against_envvar_measurements(const sgx_quote_body_t* quote_body);

tools/sgx/ra-tls/ra_tls_verify_dcap.c
@@ -118,18 +118,7 @@ int ra_tls_verify_callback(void* data, mbedtls_x509_crt* crt, int depth, uint32_
     /* extract SGX quote from "quote" OID extension from crt */
     sgx_quote_t* quote;
     size_t quote_size;
-    ret = find_oid(crt->v3_ext.p, crt->v3_ext.len, quote_oid, quote_oid_len, (uint8_t**)&quote,
-                   &quote_size);
-    if (ret < 0)
-        goto out;
-
-    if (quote_size < sizeof(*quote)) {
-        ret = MBEDTLS_ERR_X509_INVALID_EXTENSIONS;
-        goto out;
-    }
-
-    /* compare public key's hash from cert against quote's report_data */
-    ret = cmp_crt_pk_against_quote_report_data(crt, quote);
+    ret = extract_quote_and_verify_pubkey(crt, &quote, &quote_size);
     if (ret < 0)
         goto out;
 

tools/sgx/ra-tls/ra_tls_verify_epid.c
@@ -162,18 +162,7 @@ int ra_tls_verify_callback(void* data, mbedtls_x509_crt* crt, int depth, uint32_
     /* extract SGX quote from "quote" OID extension from crt */
     sgx_quote_t* quote;
     size_t quote_size;
-    ret = find_oid(crt->v3_ext.p, crt->v3_ext.len, quote_oid, quote_oid_len, (uint8_t**)&quote,
-                   &quote_size);
-    if (ret < 0)
-        goto out;
-
-    if (quote_size < sizeof(*quote)) {
-        ret = MBEDTLS_ERR_X509_INVALID_EXTENSIONS;
-        goto out;
-    }
-
-    /* compare public key's hash from cert against quote's report_data */
-    ret = cmp_crt_pk_against_quote_report_data(crt, quote);
+    ret = extract_quote_and_verify_pubkey(crt, &quote, &quote_size);
     if (ret < 0)
         goto out;
 