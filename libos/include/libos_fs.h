gramineproject/gramine

[LibOS] Clean up `select` and `poll` implementations
Signed-off-by: Borys Popławski <borysp@invisiblethingslab.com>

libos/include/libos_fs.h
@@ -21,10 +21,6 @@
 
 struct libos_handle;
 
-#define FS_POLL_RD 0x01
-#define FS_POLL_WR 0x02
-#define FS_POLL_ER 0x04
-
 /* Describes mount parameters. Passed to `mount_fs`, and to the `mount` callback. */
 struct libos_mount_params {
     /* Filesystem type (corresponds to `name` field of `libos_fs` */
@@ -182,8 +178,8 @@ struct libos_fs_ops {
     int (*checkout)(struct libos_handle* hdl);
     int (*checkin)(struct libos_handle* hdl);
 
-    /* poll a single handle */
-    int (*poll)(struct libos_handle* hdl, int poll_type);
+    /* Poll a single handle. Must not block. */
+    int (*poll)(struct libos_handle* hdl, int in_events, int* out_events);
 
     /* checkpoint/migrate the file system */
     ssize_t (*checkpoint)(void** checkpoint, void* mount_data);
@@ -950,7 +946,7 @@ int generic_readdir(struct libos_dentry* dent, readdir_callback_t callback, void
 int generic_inode_stat(struct libos_dentry* dent, struct stat* buf);
 int generic_inode_hstat(struct libos_handle* hdl, struct stat* buf);
 file_off_t generic_inode_seek(struct libos_handle* hdl, file_off_t offset, int origin);
-int generic_inode_poll(struct libos_handle* hdl, int poll_type);
+int generic_inode_poll(struct libos_handle* hdl, int events, int* out_events);
 
 int generic_emulated_mmap(struct libos_handle* hdl, void* addr, size_t size, int prot, int flags,
                           uint64_t offset);

libos/src/fs/libos_fs_util.c
@@ -116,27 +116,16 @@ file_off_t generic_inode_seek(struct libos_handle* hdl, file_off_t offset, int o
     return ret;
 }
 
-int generic_inode_poll(struct libos_handle* hdl, int poll_type) {
+int generic_inode_poll(struct libos_handle* hdl, int events, int* out_events) {
     int ret;
-
-    lock(&hdl->pos_lock);
-    lock(&hdl->inode->lock);
 
     if (hdl->inode->type == S_IFREG) {
         ret = 0;
-        if (poll_type & FS_POLL_WR)
-            ret |= FS_POLL_WR;
-        /* TODO: The `hdl->pos < hdl->inode->size` condition is wrong, the `poll` syscall treats
-         * end-of-file as readable. Check if removing this condition doesn't break anything
-         * in our `poll` implementation. */
-        if ((poll_type & FS_POLL_RD) && hdl->pos < hdl->inode->size)
-            ret |= FS_POLL_RD;
+        *out_events = events & (POLLIN | POLLRDNORM | POLLOUT | POLLWRNORM);
     } else {
         ret = -EAGAIN;
     }
 
-    unlock(&hdl->inode->lock);
-    unlock(&hdl->pos_lock);
     return ret;
 }
 

forkthus/gramine-tdx

libos/include/libos_fs.h
@@ -21,10 +21,6 @@
 
 struct libos_handle;
 
-#define FS_POLL_RD 0x01
-#define FS_POLL_WR 0x02
-#define FS_POLL_ER 0x04
-
 /* Describes mount parameters. Passed to `mount_fs`, and to the `mount` callback. */
 struct libos_mount_params {
     /* Filesystem type (corresponds to `name` field of `libos_fs` */
@@ -182,8 +178,8 @@ struct libos_fs_ops {
     int (*checkout)(struct libos_handle* hdl);
     int (*checkin)(struct libos_handle* hdl);
 
-    /* poll a single handle */
-    int (*poll)(struct libos_handle* hdl, int poll_type);
+    /* Poll a single handle. Must not block. */
+    int (*poll)(struct libos_handle* hdl, int in_events, int* out_events);
 
     /* checkpoint/migrate the file system */
     ssize_t (*checkpoint)(void** checkpoint, void* mount_data);
@@ -950,7 +946,7 @@ int generic_readdir(struct libos_dentry* dent, readdir_callback_t callback, void
 int generic_inode_stat(struct libos_dentry* dent, struct stat* buf);
 int generic_inode_hstat(struct libos_handle* hdl, struct stat* buf);
 file_off_t generic_inode_seek(struct libos_handle* hdl, file_off_t offset, int origin);
-int generic_inode_poll(struct libos_handle* hdl, int poll_type);
+int generic_inode_poll(struct libos_handle* hdl, int in_events, int* out_events);
 
 int generic_emulated_mmap(struct libos_handle* hdl, void* addr, size_t size, int prot, int flags,
                           uint64_t offset);

libos/src/fs/libos_fs_util.c
@@ -116,27 +116,16 @@ file_off_t generic_inode_seek(struct libos_handle* hdl, file_off_t offset, int o
     return ret;
 }
 
-int generic_inode_poll(struct libos_handle* hdl, int poll_type) {
+int generic_inode_poll(struct libos_handle* hdl, int in_events, int* out_events) {
     int ret;
-
-    lock(&hdl->pos_lock);
-    lock(&hdl->inode->lock);
 
     if (hdl->inode->type == S_IFREG) {
         ret = 0;
-        if (poll_type & FS_POLL_WR)
-            ret |= FS_POLL_WR;
-        /* TODO: The `hdl->pos < hdl->inode->size` condition is wrong, the `poll` syscall treats
-         * end-of-file as readable. Check if removing this condition doesn't break anything
-         * in our `poll` implementation. */
-        if ((poll_type & FS_POLL_RD) && hdl->pos < hdl->inode->size)
-            ret |= FS_POLL_RD;
+        *out_events = in_events & (POLLIN | POLLRDNORM | POLLOUT | POLLWRNORM);
     } else {
         ret = -EAGAIN;
     }
 
-    unlock(&hdl->inode->lock);
-    unlock(&hdl->pos_lock);
     return ret;
 }
 